e4dbaf01c950f1ecad9ca59cb530806752932222
Michael Chenevey <mchenevey7@gmail.com>
2022-12-25 21:48:32 -1000

    Handle a list of files, concat them. Add progress bars

--- edit.py
+++ edit.py
@@ -1,30 +1,34 @@
-import numpy as np
 import cv2
+import time
 # import human_tracker
 import encode_with_centers
 from PIL import Image
-from pykalman import KalmanFilter
+from alive_progress import alive_bar, alive_it
 from vidstab.VidStab import VidStab
 
 # Testing imports
 from perlin_noise import PerlinNoise
 
 FILE = 'video/quad-test.mp4'
-noise = PerlinNoise(octaves=6)
+FILES = [
+    'video/BREAKERSvVIPERS.1.1.MP4',
+    'video/BREAKERSvVIPERS.1.2.MP4',
+    'video/BREAKERSvVIPERS.1.3.MP4',
+    'video/BREAKERSvVIPERS.1.4.MP4',
+]
+SHOW_VIDEO = False
 
-# Open the video
-cap = cv2.VideoCapture(FILE)
+noise = PerlinNoise(octaves=6)
 
 # Initialize filters
 bg = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
 stabilizer = VidStab()
+smoothing_window = 30
 
-# Initialize frame counter
-cnt = 0
-
-# Some characteristics from the original video
-w_frame, h_frame = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps, frames = cap.get(cv2.CAP_PROP_FPS), cap.get(cv2.CAP_PROP_FRAME_COUNT)
+# Open video and get some characteristics
+caps = [cv2.VideoCapture(file) for file in FILES]
+w_frame, h_frame = int(caps[0].get(cv2.CAP_PROP_FRAME_WIDTH)), int(caps[0].get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps, frames = caps[0].get(cv2.CAP_PROP_FPS), caps[0].get(cv2.CAP_PROP_FRAME_COUNT)
 
 # Output screen size
 h,w = 720,1280
@@ -72,51 +76,50 @@ def get_stabilized_center(cnt, frame):
     average = [sum(x)//len(x) for x in zip(*centers)]
     return average, center
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
+for index, cap in enumerate(caps):
+    print(f'File {index + 1} of {len(caps)}')
+    # Initialize frame counter
+    cnt = 0
+    with alive_bar(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))) as bar:
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            cnt += 1
+            bar()
 
-    cnt += 1 # Counting frames
+            # Avoid problems when video finish
+            if ret==True:
+                # Stabilize frame (destructive)
+                frame = stabilizer.stabilize_frame(input_frame=frame, smoothing_window=smoothing_window)
 
-    # Avoid problems when video finish
-    if ret==True:
-        # Stabilize frame (destructive)
-        frame = stabilizer.stabilize_frame(input_frame=frame, smoothing_window=30)
+                # Perform background subtraction
+                bg_removed = bg.apply(frame)
 
-        # Perform background subtraction
-        bg_removed = bg.apply(frame)
+                # Get attention center
+                stab_center, actual_center = get_stabilized_center(cnt, bg_removed)
+                center_x, center_y = stab_center
+                y, x = get_corner_from_roi(center_y, center_x)
 
-        # Get attention center
-        stab_center, actual_center = get_stabilized_center(cnt, bg_removed)
-        center_x, center_y = stab_center
-        y, x = get_corner_from_roi(center_y, center_x)
+                # Crop the frame using attention center
+                crop_frame = frame[y:y+h, x:x+w].copy()
 
-        # Crop the frame using attention center
-        crop_frame = frame[y:y+h, x:x+w].copy()
-        # Percentage
-        xx = cnt *100/frames
-        print(int(xx),'%')
+                out.write(crop_frame)
 
-        out.write(crop_frame)
+                # humans
+                # human_tracker.get_humans(frame, cnt)
 
-        # humans
-        # human_tracker.get_humans(frame, cnt)
+                if SHOW_VIDEO:
+                    # Just to see the video in real time
+                    cv2.circle(frame, stab_center, 50, (255, 0, 0), 5)
+                    cv2.circle(frame, actual_center, 15, (0, 255, 0), 5)
+                    cv2.imshow('frame',frame)
+                    cv2.imshow('cropped',crop_frame)
 
-        # Just to see the video in real time
-        cv2.circle(frame, stab_center, 50, (255, 0, 0), 5)
-        cv2.circle(frame, actual_center, 15, (0, 255, 0), 5)
-        cv2.imshow('frame',frame)
-        cv2.imshow('cropped',crop_frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    print("Found break key")
+                    break
+            elif cnt > frames * 1.1:
+                break
+        cap.release()
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            print("Found break key")
-            break
-    elif cnt > frames * 1.5:
-        break
-    else:
-        cnt += 1
-        print("invalid frame?")
-
-
-cap.release()
 out.release()
 cv2.destroyAllWindows()
